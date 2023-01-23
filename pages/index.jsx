e007d60be4a5397f4ea74e42720d1e52711b2445
githubforkj <kajihara.ryoga.qd@alumni.tsukuba.ac.jp>
2023-01-23 23:47:27 +0900

    3枚のカードによって画像の出し分けを可能にした

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,7 +8,9 @@ export default function Home() {
   // useStateを使う.APIの返り値
   const [mangaInput, setMangaInput] = useState("");
   const [result, setResult] = useState();
-  const [img_url, setUrl] = useState();
+  const [img_url_1, setUrl1] = useState('画像を作ってね!');
+  const [img_url_2, setUrl2] = useState('画像を作ってね!');
+  const [img_url_3, setUrl3] = useState('画像を作ってね!');
 
   // タイトル生成ボタンを押した後
   async function generateTitle(event) {
@@ -39,38 +41,135 @@ export default function Home() {
     }
   }
 
+  // // img_urlの定義
+  // let img_url_1 = "画像を作ってね!";
+  // let img_url_2 = "画像を作ってね!";
+  // let img_url_3 = "画像を作ってね!";
+
   // 画像生成ボタンを押した後
-  const generateImage = async (event) => {
+  const generateImage = (index) => async (event) => {
     event.preventDefault();
     try {
-      console.log('a')
-      const response = await fetch("/api/generateImage", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        // que:generateImage.jsの中の変数 プロンプトはres.firstを使用
-        body: JSON.stringify({ que: res.first }),
-      });
-      console.log('b')
-      const data = await response.json();
-      console.log('c')
-      if (response.status !== 200) {
-        throw (
-          data.error ||
-          new Error(`Request failed with status ${response.status}`)
-        );
-      }
-      console.log("response.status", response.status)
-      // console.log("response", response);
-      console.log("data", data);
-      console.log("data.result[0].url", data.result[0].url);
-      console.log("d")
+      if (index == 1) {
+        console.log('index:', 1)
+        console.log('a')
+        const response = await fetch("/api/generateImage", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          // que:generateImage.jsの中の変数 プロンプトはres.firstを使用
+          body: JSON.stringify({ que: res.first }),
+        });
+        console.log("response:", response)
+        console.log('b')
+        const data = await response.json();
+        console.log('c')
+        if (response.status !== 200) {
+          throw (
+            data.error ||
+            new Error(`Request failed with status ${response.status}`)
+          );
+        }
+        console.log("response.status", response.status)
+        // console.log("response", response);
+        console.log("data", data);
+        console.log("data.result[0].url", data.result[0].url);
+        console.log("d")
 
-      // console.log("response.data.data[0].url:", response.data.data[0].url);
-      console.log("img_url:", img_url)
-      console.log("dd")
-      setUrl(data.result[0].url);
+        // console.log("response.data.data[0].url:", response.data.data[0].url);
+        console.log("img_url1:", img_url_1)
+        console.log("dd")
+        setUrl1(data.result[0].url);
+        console.log("img_url1:", img_url_1)
+      } else if (index == 2) {
+        console.log('index:', 2)
+        console.log('a')
+        const response = await fetch("/api/generateImage", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          // que:generateImage.jsの中の変数 プロンプトはres.firstを使用
+          body: JSON.stringify({ que: res.second }),
+        });
+        console.log("response:", response)
+        console.log('b')
+        const data = await response.json();
+        console.log('c')
+        if (response.status !== 200) {
+          throw (
+            data.error ||
+            new Error(`Request failed with status ${response.status}`)
+          );
+        }
+        console.log("response.status", response.status)
+        // console.log("response", response);
+        console.log("data", data);
+        console.log("data.result[0].url", data.result[0].url);
+        console.log("d")
+
+        // console.log("response.data.data[0].url:", response.data.data[0].url);
+        console.log("img_url_2:", img_url_2)
+        console.log("dd")
+        setUrl2(data.result[0].url);
+        console.log("img_url_2:", img_url_2)
+      } else {
+        console.log('index:', 3)
+        console.log('a')
+        const response = await fetch("/api/generateImage", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          // que:generateImage.jsの中の変数 プロンプトはres.firstを使用
+          body: JSON.stringify({ que: res.third }),
+        });
+        console.log("response:", response)
+        console.log('b')
+        const data = await response.json();
+        console.log('c')
+        if (response.status !== 200) {
+          throw (
+            data.error ||
+            new Error(`Request failed with status ${response.status}`)
+          );
+        }
+        console.log("response.status", response.status)
+        // console.log("response", response);
+        console.log("data", data);
+        console.log("data.result[0].url", data.result[0].url);
+        console.log("d")
+
+        // console.log("response.data.data[0].url:", response.data.data[0].url);
+        console.log("img_url_3:", img_url_3)
+        console.log("dd")
+        setUrl3(data.result[0].url);
+        console.log("img_url_3:", img_url_3)
+      };
+
+
+      // console.log("response:", response)
+      // console.log('b')
+      // const data = await response.json();
+      // console.log('c')
+      // if (response.status !== 200) {
+      //   throw (
+      //     data.error ||
+      //     new Error(`Request failed with status ${response.status}`)
+      //   );
+      // }
+      // console.log("response.status", response.status)
+      // // console.log("response", response);
+      // console.log("data", data);
+      // console.log("data.result[0].url", data.result[0].url);
+      // console.log("d")
+
+      // // console.log("response.data.data[0].url:", response.data.data[0].url);
+      // console.log("img_url:", img_url)
+      // console.log("dd")
+      // setUrl(data.result[0].url);
+
     } catch (error) {
       // Consider implementing your own error handling logic here
       console.error("通信に失敗したよ", error);
@@ -112,7 +211,9 @@ export default function Home() {
 
   console.log("a1:");
   // 生成した画像のurlを格納する変数を定義
-  console.log(img_url);
+  console.log("img_url_1", img_url_1);
+  console.log("img_url_2", img_url_2);
+  console.log("img_url_3", img_url_3);
   console.log("a2:");
   console.log(result);
   console.log("a3:");
@@ -149,12 +250,12 @@ export default function Home() {
           </form>
         </Flex>
       </Flex >
-      <p>画像のurl:{img_url}</p>
+      {/* <p>画像のurl:{img_url}</p> */}
       {/* 結果を表示するエリア */}
       <Flex height="60vh" align="center" justify="space-around" direction="row" background="gray.50" p={8} m={8}>
-        <ResultCards GI={generateImage} MT={res.first} url={img_url} tweet={tweet_text.first}></ResultCards>
-        <ResultCards GI={generateImage} MT={res.second} url={img_url} tweet={tweet_text.second}></ResultCards>
-        <ResultCards GI={generateImage} MT={res.third} url={img_url} tweet={tweet_text.third}></ResultCards>
+        <ResultCards GI={generateImage(1)} MT={res.first} url={img_url_1} tweet={tweet_text.first}></ResultCards>
+        <ResultCards GI={generateImage(2)} MT={res.second} url={img_url_2} tweet={tweet_text.second}></ResultCards>
+        <ResultCards GI={generateImage(3)} MT={res.third} url={img_url_3} tweet={tweet_text.third}></ResultCards>
       </Flex>
     </>
   );
