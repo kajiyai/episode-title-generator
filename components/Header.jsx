763053f10668ab6e6fecfd4eb0216c729d87fd2e
kajiyai <kajihara.ryoga.qd@alumni.tsukuba.ac.jp>
2023-01-29 17:47:44 +0900

    HeaderのOGP等を変更

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,13 +3,13 @@ import Head from "next/head";
 const Header = () => {
     return (
         <Head >
-            <title>Manga Title Generater</title>
-            <meta property="og:title" content="Manga-Title-Generator" />
+            <title>エピタイ!!</title>
+            <meta property="og:title" content="エピタイ!!" />
             <meta property="og:type" content="website" />
             <meta property="og:url" content="https://manga-title-generator.vercel.app/" />
-            <meta property="og:image" content="../public/img/idea.jpg" />
-            <meta property="og:site_name" content="Manga-Title-Generator" />
-            <meta property="og:description" content="You can generate unique manga title!! This fits Twitter." />
+            <meta property="og:image" content="/img/ai1.jpg" />
+            <meta property="og:site_name" content="epiti!!" />
+            <meta property="og:description" content="You can make episode title and suited image for your manga or comic." />
             <meta name="twitter:card" content="summary_large_image" />
         </Head>
     )
