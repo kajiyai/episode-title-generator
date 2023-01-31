e7c056f781638cae21940aab066b6bc365655569
kajiyai <kajihara.ryoga.qd@alumni.tsukuba.ac.jp>
2023-01-31 15:11:41 +0900

    component copytextの削除

--- components/Features.jsx
+++ components/Features.jsx
@@ -79,18 +79,6 @@ export default function Features(props) {
                                 borderColor={useColorModeValue('gray.100', 'gray.700')}
                             />
                         }>
-                        {/* <Feature
-                            icon={
-                                <Icon as={IoAnalyticsSharp} color={'yellow.500'} w={5} h={5} />
-                            }
-                            iconBg={useColorModeValue('yellow.100', 'yellow.900')}
-                            text={'Super'}
-                        />
-                        <Feature
-                            icon={<Icon as={IoLogoBitcoin} color={'green.500'} w={5} h={5} />}
-                            iconBg={useColorModeValue('green.100', 'green.900')}
-                            text={'Ultra'}
-                        /> */}
                         <Feature
                             icon={
                                 <Icon as={IoSearchSharp} color={'purple.500'} w={5} h={5} />
